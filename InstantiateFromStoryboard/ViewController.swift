773ed8618c8e932a1496e752f785fc18c4b8ae92
Eugenio Baglieri <baglieri.eugenio@gmail.com>
2015-12-30 21:39:18 +0100

    Initial Commit

---
+++ InstantiateFromStoryboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstantiateFromStoryboard
+//
+//  Created by Eugenio Baglieri on 30/12/15.
+//  Copyright © 2015 Eugenio Baglieri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
